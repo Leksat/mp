5610174ad17e6feb491a60d36360b6d966b62b53
Alex Tkachev <alex.leksat@gmail.com>
2026-09-20 23:00:28 +0400

    fix: block landscape with a hint instead of rotating the layout

--- src/orientation.ts
+++ src/orientation.ts
@@ -1,9 +1,3 @@
-const applyAngle = (): void => {
-  document.documentElement.dataset.angle = String(screen.orientation?.angle ?? 0)
-}
-
 export const holdPortrait = (): void => {
   void screen.orientation?.lock?.('portrait').catch(() => {})
-  applyAngle()
-  screen.orientation?.addEventListener('change', applyAngle)
 }
